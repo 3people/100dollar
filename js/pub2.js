4cd18bf08e28c6131bfd74da1ead1925de6664fc
이세명 <semyeonglee@isemyeong-ui-MacBookPro.local>
2019-11-29 16:30:23 +0900

    Apply .gitignore

--- js/pub2.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $("#keyword").keyup(function() {
-        var k = $(this).val();
-        $("#contents > div > span").hide();
-        var temp = $("#contents > div > span > p:contains('" + k + "')");
-
-        $(temp).parent().show();
-    })
-})
